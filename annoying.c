99bf05030a0caa04465ed6549c0ca5dd2c77a16e
powcoder <powcoder@163.com>
2022-11-23 12:36:33 +0800

    CSCI 4210 OS HW02

---
+++ annoying.c
@@ -0,0 +1,20 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+/* annoying.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+  while ( 1 )
+  {
+    printf( "Hey, get back to work!\n" );
+    sleep( 3 );
+  }
+
+  return EXIT_SUCCESS;
+}
